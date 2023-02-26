de1fda8a9acc706a01fa4be6f7be07750bd5ac5a
albuch <albuch@users.noreply.github.com>
2023-02-26 13:26:03 +0100

    :rotating_light: Fix codacy warnings

--- src/main/scala/net/vonbuchholtz/sbt/dependencycheck/DependencyCheckUpdateTask.scala
+++ src/main/scala/net/vonbuchholtz/sbt/dependencycheck/DependencyCheckUpdateTask.scala
@@ -3,12 +3,14 @@ package net.vonbuchholtz.sbt.dependencycheck
 import org.owasp.dependencycheck.Engine
 import sbt.Logger
 
+import scala.util.control.NonFatal
+
 object DependencyCheckUpdateTask {
   def update(engine: Engine, log: Logger): Unit = {
     try {
       engine.doUpdates()
     } catch {
-      case e: Exception =>
+      case e: Exception if NonFatal(e) =>
         log.error(s"An exception occurred connecting to the local database: ${e.getLocalizedMessage}")
         throw e
     }
